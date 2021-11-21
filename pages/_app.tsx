44b9b96fe92e42aa0dce09d04bf84b86c96a8143
vaib1343 <vaib1343@gmail.com>
2021-11-21 11:14:03 +0530

    store config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
 import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { store } from '../redux/store'
+import { Provider } from 'react-redux'
+function MyApp({ Component, pageProps }: AppProps) {
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (<Provider store={store}><Component {...pageProps} /></Provider>)
 }
 
 export default MyApp
